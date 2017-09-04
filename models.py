e62d53b6b48c92bf9028b1d59124ce016ad282a7
Cray <cray.huang@gmail.com>
2017-09-04 21:24:22 +0800

    change model string to text when the string length is larger than 200

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, String, Numeric, Integer
+from sqlalchemy import Column, String, Numeric, Integer, Text
 from database import Base
 
 class User(Base):
 	__tablename__ = 'users'
 	id = Column(Integer, primary_key=True)
 	nickname = Column(String(100))
-	open_id = Column(String(200), unique=True)
+	open_id = Column(Text(200), unique=True)
 	def __init__(self, nickename=None, open_id=None):
 		self.nickname = nickename
 		self.open_id = open_id
@@ -24,17 +24,17 @@ class User(Base):
 class ParkingInfo(Base):
 	__tablename__ = 'parking_info'
 	id = Column(Integer, primary_key=True, autoincrement=True)
-	name = Column(String(500))
-	description = Column(String(2000))
+	name = Column(Text(500))
+	description = Column(Text(2000))
 	province = Column(String(20))
 	city = Column(String(20))
 	district = Column(String(20))
-	address = Column(String(2000))
+	address = Column(Text(2000))
 	longitude = Column(Numeric(16, 10))
 	latitude = Column(Numeric(16, 10))
-	fee = Column(String(500))
-	remark = Column(String(2000))
-	opening_time = Column(String(200))
+	fee = Column(Text(500))
+	remark = Column(Text(2000))
+	opening_time = Column(Text(200))
 	telephone = Column(String(100))
 
 	def __init__(self, name=None, description=None, province=None, city=None, district=None, address=None, longitude=0.0, latitude=0.0, 
